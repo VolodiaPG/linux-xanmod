9ae4844bdc5b637d68b432366a4ab5a09cef3c97
kevall474 <kevall474@tuta.io>
2021-10-14 17:31:21 -0400

    Update build.sh

--- stable/build.sh
+++ stable/build.sh
@@ -16,14 +16,14 @@ rm -rf pkg src
 
 # clang
 
-env _compiler=2 makepkg -s
+#env _compiler=2 makepkg -s
 
-rm -rf pkg src
+#rm -rf pkg src
 
-env _cpu_sched=1 _compiler=2 makepkg -s
+#env _cpu_sched=1 _compiler=2 makepkg -s
 
-rm -rf pkg src
+#rm -rf pkg src
 
-env _cpu_sched=2 _compiler=2  makepkg -s
+#env _cpu_sched=2 _compiler=2  makepkg -s
 
-rm -rf pkg src
+#rm -rf pkg src
