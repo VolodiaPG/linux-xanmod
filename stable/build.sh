3a6bc3d3db3829140ab674729580a73bcacad5c1
kevall474 <kevall474@tuta.io>
2021-12-20 11:23:48 -0500

    Update build.sh

--- stable/build.sh
+++ stable/build.sh
@@ -10,6 +10,10 @@ env _cpu_sched=1 makepkg -s
 
 rm -rf pkg src
 
+env _cpu_sched=2 makepkg -s
+
+rm -rf pkg src
+
 # clang
 
 #env _compiler=2 makepkg -s
@@ -17,5 +21,9 @@ rm -rf pkg src
 #rm -rf pkg src
 
 #env _cpu_sched=1 _compiler=2 makepkg -s
+
+#rm -rf pkg src
+
+#env _cpu_sched=2 _compiler=2 makepkg -s
 
 #rm -rf pkg src
