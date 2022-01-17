c7722bc9d476a946e14cd62e7692489810dd8044
kevall474 <kevall474@tuta.io>
2022-01-16 23:55:05 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,24 +4,24 @@
 
 # gcc
 
-cd lts && makepkg -s && rm -rf pkg/ src/ && cd ..
+cd edge && makepkg -s && rm -rf pkg/ src/ && cd ..
 
 cd stable && makepkg -s && rm -rf pkg/ src/ && cd ..
 
 cd tt && makepkg -s && rm -rf pkg/ src/ && cd ..
 
-cd bore && makepkg -s && rm -rf pkg/ src/ && cd ..
+cd lts && makepkg -s && rm -rf pkg/ src/ && cd ..
 
-cd edge && makepkg -s && rm -rf pkg/ src/ && cd ..
+cd bore && makepkg -s && rm -rf pkg/ src/ && cd ..
 
 # clang
 
-#cd lts && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
+#cd edge && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
 
 #cd stable && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
 
 #cd tt && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
 
-#cd bore && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
+#cd lts && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
 
 #cd edge && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
