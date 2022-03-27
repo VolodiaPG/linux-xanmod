31bdd4eb61d95e3ae209234ea0b5eb1c8043ef99
blacksky3 <blacksky3@tuta.io>
2022-03-27 18:39:30 -0400

    update

--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@
 
 # gcc
 
+# official package
+
 cd edge && makepkg -s && rm -rf pkg/ src/ && cd ..
 
 cd stable && makepkg -s && rm -rf pkg/ src/ && cd ..
@@ -11,6 +13,8 @@ cd stable && makepkg -s && rm -rf pkg/ src/ && cd ..
 cd tt && makepkg -s && rm -rf pkg/ src/ && cd ..
 
 cd lts && makepkg -s && rm -rf pkg/ src/ && cd ..
+
+# non-official package
 
 cd tt-edge && makepkg -s && rm -rf pkg/ src/ && cd ..
 
@@ -22,7 +26,13 @@ cd cacule-edge && makepkg -s && rm -rf pkg/ src/ && cd ..
 
 cd cacule-rdb-edge && makepkg -s && rm -rf pkg/ src/ && cd ..
 
+cd cacule && makepkg -s && rm -rf pkg/ src/ && cd ..
+
+cd cacule-rdb && makepkg -s && rm -rf pkg/ src/ && cd ..
+
 # clang
+
+# official package
 
 #cd edge && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
 
@@ -32,8 +42,18 @@ cd cacule-rdb-edge && makepkg -s && rm -rf pkg/ src/ && cd ..
 
 #cd lts && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
 
+# non-official package
+
 #cd tt-edge && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
 
 #cd bore-edge && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
 
 #cd bore && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
+
+#cd cacule-edge && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
+
+#cd cacule-rdb-edge && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
+
+#cd cacule && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
+
+#cd cacule-rdb && env _compiler=2 makepkg -s && rm -rf pkg/ src/ && cd ..
